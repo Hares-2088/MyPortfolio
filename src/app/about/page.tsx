77292d2442ab9ce586ef1dc15d453e93c1fa828d
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2025-01-21 18:14:40 -0500

    Deleted unnecessary folders

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>About Me</h1>
-      <p>This is the about page.</p>
-    </div>
-  );
-}
--- src/app/projects/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectPage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Project {params.id}</h1>
-      <p>This is the project details page.</p>
-    </div>
-  );
-}
